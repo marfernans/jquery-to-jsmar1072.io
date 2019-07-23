2dad662a2f73c312162aff2b3c01fce075aa66ca
MARIO FDEZ <mario.mamonuo@gmail.com>
2019-07-23 17:08:12 -0500

    Creación del DOM

--- src/js/home.js
+++ src/js/home.js
@@ -74,15 +74,68 @@ fetch("https://randomuser.me/api/hihoj")
   //drama
   //Animación
   async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
   }
 
+  const actionList = await getData(
+    "https://yts.lt/api/v2/list_movies.json?genre=action"
+  );
+  const dramaList = await getData(
+    "https://yts.lt/api/v2/list_movies.json?genre=drama"
+  );
+  const animationList = await getData(
+    "https://yts.lt/api/v2/list_movies.json?genre=animation"
+  );
+  console.log(actionList, dramaList, animationList);
+  // debugger
+  function videoItemTemplate(movie) {
+    return (`<div class="primaryPlaylistItem">
+           <div class="primaryPlaylistItem-image">
+             <img src="${movie.medium_cover_image}">
+           </div>
+           <h4 class="primaryPlaylistItem-title">
+             ${movie.title}
+           </h4>
+         </div>`
+   )}
 
-  const actionList = await getData("https://yts.lt/api/v2/list_movies.json?genre=action");
-  const dramaList = await getData("https://yts.lt/api/v2/list_movies.json?genre=drama");
-  const animationList = await getData("https://yts.lt/api/v2/list_movies.json?genre=animation");
-  console.log(actionList, dramaList, animationList)
- 
+  //console.log (videoItemTemplate('src/images/covers/bitcoin.jpg', 'bitcoin'));
+  const $actionContainer = document.querySelector("#action");
+  actionList.data.movies.forEach((movie) => {
+    //debugger
+
+    const HTMLString = videoItemTemplate(movie);
+    const html = document.implementation.createHTMLDocument();
+    html.body.innerHTML = HTMLString; 
+    // debugger
+    $actionContainer.append(html.body.children[0]);
+    console.log(HTMLString);
+  })
+
+  const $dramaContainer = document.getElementById("#drama");
+  const $animationContainer = document.getElementById("#anomation");
+
+  const $featuringContainer = document.getElementById("#featuring");
+  const $form = document.getElementById("#form");
+  const $home = document.getElementById("#home");
+
+  // const $home = $('.home, .list #item');
+  const $modal = document.getElementById("modal");
+  const $overlay = document.getElementById("overlay");
+  const $hideModal = document.getElementById("hide-modal");
+
+  const $modalTitle = $modal.querySelector("h1");
+  const $modalImage = $modal.querySelector("img");
+  const $modalDescription = $modal.querySelector("p");
+
+  //   '<div class="primaryPlaylistItem">'
+  //   '<div class="primaryPlaylistItem-image">'
+  //     '<img src="src/images/covers/midnight.jpg">'
+  //   '</div>'
+  //   '<h4 class="primaryPlaylistItem-title">'
+  //     //Titulo de la peli
+  //   '</h4>'
+  // '</div>'
 })();
